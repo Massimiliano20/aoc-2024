fee3bf3c60a1175a20144b3f8ea849b7aaf5ecfc
Massimiliano Righetti <righetti.massi@gmail.com>
2024-12-03 23:12:20 +0100

    feat: day 3 part 2 of AoC 2024

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,14 +7,26 @@ fun main() {
         }.sum()
     }
 
-    fun part2(input: List<String>): Int = 0
+    fun part2(input: List<String>): Int {
+        var enabled = true
+        return input.sumOf {
+            Regex("""mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do\(\))|(don't\(\))""").findAll(it).map {
+                enabled = when(it.value) {
+                    "do()" -> true
+                    "don't()" -> false
+                    else -> return@map if(enabled)(it.groups[1]?.value?.toInt() ?: 0) * (it.groups[2]?.value?.toInt() ?: 0) else 0
+                }
+                0
+            }.sum()
+        }
+    }
 
-    check(part1(readInputFormatted("Day03-test")) == 161)
-//    check(part2(readInputFormatted("Day02-test")) == 4)
+    check(part1(readInputFormatted("Day03-test-part1")) == 161)
+    check(part2(readInputFormatted("Day03-test-part2")) == 48)
 
     val input = readInputFormatted("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
 private fun readInputFormatted(file: String) = readInput(file)
