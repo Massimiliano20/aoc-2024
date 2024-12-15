1fb677f41f1bb5964ef0bdabee4e148211760b80
Massimiliano Righetti <righetti.massi@gmail.com>
2024-12-15 22:24:56 +0100

    feat: day 5 part 2 of AoC 2024

--- src/Day05.kt
+++ src/Day05.kt
@@ -6,14 +6,26 @@ fun main() {
         else 0
     }
 
-    fun part2(input: InputDay04): Int = 0
+    fun part2(input: InputDay04): Int = input.inputValues.sumOf { data ->
+        var brokenRule = input.rules.find {
+            !(data.indexOf(it.first) == -1 || data.indexOf(it.second) == -1 || data.indexOf(it.first) < data.indexOf(it.second))}
+        if(brokenRule == null) return@sumOf 0
+        val mutableData = data.toMutableList()
+        do {
+            mutableData.remove(brokenRule?.first)
+            mutableData.add(mutableData.indexOf(brokenRule?.second), brokenRule?.first ?: 0)
+            brokenRule = input.rules.find {
+                !(mutableData.indexOf(it.first) == -1 || mutableData.indexOf(it.second) == -1 || mutableData.indexOf(it.first) < mutableData.indexOf(it.second))}
+        } while (brokenRule != null)
+        mutableData[data.size / 2]
+    }
 
     check(part1(readInputFormatted("Day05-test")) == 143)
-//    check(part2(readInputFormatted("Day05-test")) == 9)
-//
+    check(part2(readInputFormatted("Day05-test")) == 123)
+
     val input = readInputFormatted("Day05")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
 private fun readInputFormatted(file: String): InputDay04 {
